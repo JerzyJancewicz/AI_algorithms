815034cbf9a1ab7389e10c0bcdd9c5e6d3a5c729
JerzyJancewicz <jancewiczjerzy@gmail.com>
2023-03-22 13:41:12 +0100

    created knn object with PredictedLabelKnn and KNearest.

--- knn/knn/Program.cs
+++ knn/knn/Program.cs
@@ -10,6 +10,10 @@ namespace knn
     {
         public static void Main(string[] args)
         {
+            List<string> data = File.ReadAllLines("J:\\NAI_ai\\NAI_ai\\knn\\knn\\data\\iris.data").ToList();
+            List<string> testData = File.ReadAllLines("J:\\NAI_ai\\NAI_ai\\knn\\knn\\data\\iris.test.data").ToList();
+            Knn knn = new Knn();
+            knn.PredictedLabelKnn(data, testData, 3);
         }
     }
 }
--- knn/knn/knn.cs
+++ knn/knn/knn.cs
@@ -5,19 +5,40 @@ using System.Linq;
 
 namespace knn
 {
-    public class knn
+    public class Knn
     {
-        private List<string> data = File.ReadAllLines("J:\\NAI_ai\\NAI_ai\\knn\\knn\\data\\iris.data").ToList();
-
-        public void predicted_label_knn(List<double> data, List<double> testData, int k)
+        public void PredictedLabelKnn(List<string> data, List<string> testData, int k)
         {
+            List<string> values = new List<string>();
             List<double> distances = new List<double>();
             double distance = 0;
-            foreach (var VARIABLE in data)
+            foreach (string line in testData)
             {
-                distance = Math.Sqrt(2);
-                distances.Add(VARIABLE);
+                values = line.Split(',').ToList();
+                values.Remove(values[values.Count - 1]);
+                foreach (string line2 in data)
+                {
+                    distance = Math.Sqrt(2);
+                    distances.Add(distance);
+                }
+                
+                //KNearest(k, distances);
+                // metoda k najblizszych
+                // wyswietlenie jaki gatunek
+                // distance = Math.Sqrt(2);
             }
         }
+
+        private List<double> KNearest(int k , List<double> distances)
+        {
+            List<double> tmp = new List<double>();
+            distances.Sort();
+            for (int i = 0; i < k; i++)
+            {
+                tmp.Add(distances[i]);
+            }
+            return tmp;
+        }
+        
     }
 }
