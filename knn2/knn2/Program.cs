1a072ad69edda59071ad28aa5c043149261e7569
JerzyJancewicz <jancewiczjerzy@gmail.com>
2023-03-27 20:26:40 +0200

    End of the project

--- knn2/knn2/Program.cs
+++ knn2/knn2/Program.cs
@@ -4,9 +4,11 @@
     {
         public static void Main(string[] args)
         {
-            List<string> data = File.ReadAllLines("J:\\NAI_ai\\NAI_ai\\knn2\\knn2\\data\\iris.data").ToList();
-            List<string> testData = File.ReadAllLines("J:\\NAI_ai\\NAI_ai\\knn2\\knn2\\data\\iris.test.data").ToList();
-            Knn knn = new Knn(data, testData, 3);
+            List<string> data = File.ReadAllLines("C:\\Users\\jance\\NAI_Projekty\\NAI_ai\\knn2\\knn2\\data\\iris.data").ToList();
+            List<string> testData = File.ReadAllLines("C:\\Users\\jance\\NAI_Projekty\\NAI_ai\\knn2\\knn2\\data\\iris.test.data").ToList();
+            List<string> data2 = File.ReadAllLines("C:\\Users\\jance\\NAI_Projekty\\NAI_ai\\knn2\\knn2\\data\\wdbc.data").ToList();
+            List<string> testData2 = File.ReadAllLines("C:\\Users\\jance\\NAI_Projekty\\NAI_ai\\knn2\\knn2\\data\\wdbc.test.data").ToList();
+            Knn knn = new Knn(data2, testData2, 3);
             knn.ShowKnnAccuracy();
             Console.WriteLine();
             
