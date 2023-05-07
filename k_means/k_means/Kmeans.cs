04c2da98fea5a8ac6e9279e7c45c010284a0d6a8
JerzyJancewicz <jancewiczjerzy@gmail.com>
2023-05-07 16:05:32 +0200

    k_means

---
+++ k_means/k_means/Kmeans.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace k_means;
+
+public class Kmeans
+{
+    private int _k;
+    private readonly List<string> _data;
+    private List<List<double>> centroids;
+
+    public Kmeans(int k ,List<string> data)
+    {
+        _k = k;
+        _data = data;
+        GenerateCentroids();
+    }
+
+    private void GenerateCentroids()
+    {
+        centroids = new List<List<double>>();
+        for (int i = 0; i < _k; _k++)
+        {
+            centroids.Add(ParseDoubles(_data[new Random().Next(_data.Count - 1)]));
+        }
+    }
+
+    private List<double> ParseDoubles(string line)
+    {
+        List<string> values = line.Split(',').ToList();
+        values.Remove(values[values.Count - 1]);
+        return values.Select(s => double.Parse(s,NumberStyles.Any, CultureInfo.InvariantCulture)).ToList();
+    }
+    
+}
---
+++ k_means/k_means/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace k_means
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> data = File.ReadAllLines("J:\\NAI_ai\\NAI_ai\\k_means\\k_means\\iris.data").ToList();
+            Kmeans kmeans = new Kmeans(4, data);
+        }
+    }
+}
