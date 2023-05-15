7d05de5382658c4caf6cde5b74febb1e439dec47
JerzyJancewicz <jancewiczjerzy@gmail.com>
2023-05-15 16:55:21 +0200

    End of task

--- k_means/k_means/Kmeans.cs
+++ k_means/k_means/Kmeans.cs
@@ -7,6 +7,8 @@ public class Kmeans
     private readonly int _k;
     private readonly List<string> _data;
     private List<string> _centroids;
+    private double _sum;
+    private Dictionary<int, List<List<double>>> _groupedLines;
 
     public Kmeans(int k ,List<string> data)
     {
@@ -15,20 +17,74 @@ public class Kmeans
         GenerateCentroids();
     }
 
+    // accumulate all functions
+    // making algorithm until 2 groups would be the same
     public void DoKmeans()
     {
-        
+        Dictionary<int, List<List<double>>> tmpDictionary = new Dictionary<int, List<List<double>>>();
+        bool haveSameNumberOfLists = false;
+        while (!haveSameNumberOfLists)
+        {
+            GroupDistances();
+            _centroids = NewCentroids(_groupedLines);
+            haveSameNumberOfLists = AreInTheSameGroups(_groupedLines, tmpDictionary);
+            tmpDictionary = _groupedLines;
+            Console.WriteLine(_sum);
+            _sum = 0;
+        }
+        ShowSquads();
     }
 
-    // indexAndValue to dany centroid z najmniejsza wartosscia
-    // trzeba dodac go do tej grupy o danym indeksie co jest centroid 
+    private void ShowSquads()
+    {
+        foreach (var pair in _groupedLines)
+        {
+            int groupIndex = pair.Key;
+            List<List<double>> groupList = pair.Value;
+
+            Console.WriteLine("Grupa " + groupIndex + " sklad: ");
+            foreach (var group in groupList)
+            {
+                Console.WriteLine(string.Join(", ", group));
+            }
+            Console.WriteLine();
+        }
+    }
+
+    // checking dictionaries if the number of Lists in each group is the same as in previous dictionary
+    private bool AreInTheSameGroups(Dictionary<int, List<List<double>>> firstDictionary, Dictionary<int, List<List<double>>> secondDictionary)
+    {
+        bool haveSameNumberOfLists = true;
+
+        foreach (var pair in firstDictionary)
+        {
+            int key = pair.Key;
+            List<List<double>> firstGroupLists = pair.Value;
+
+            if (secondDictionary.TryGetValue(key, out List<List<double>> secondGroupLists))
+            {
+                if (firstGroupLists.Count != secondGroupLists.Count)
+                {
+                    haveSameNumberOfLists = false;
+                    break;
+                }
+            }
+            else
+            {
+                haveSameNumberOfLists = false;
+                break;
+            }
+        }
+        return haveSameNumberOfLists;
+    }
+
+    // grouping distances and adding them to dictionary in the correct order and form 
     private void GroupDistances()
     {
         List<List<double>> allCentroids = new List<List<double>>();
         List<double> distances = new List<double>();
-        int index = 0;
-        List<double> indexAndValue;
-        
+        _groupedLines = new Dictionary<int, List<List<double>>>();
+
         foreach (var centroid in _centroids)
         {
             allCentroids.Add(Distances(centroid));
@@ -38,13 +94,48 @@ public class Kmeans
         {
             for (int j = 0; j < allCentroids.Count; j++)
             {
-                distances.Add(allCentroids[i][j]);
+                distances.Add(allCentroids[j][i]);
             }
-            indexAndValue = SortWithIndex(distances);
+            var indexAndValue = SortWithIndex(distances);
+            _sum += indexAndValue[0];
+            
+            int centroidIndex = (int)indexAndValue[1];
+            if (!_groupedLines.ContainsKey(centroidIndex))
+            {
+                _groupedLines[centroidIndex] = new List<List<double>>();
+            }
+            _groupedLines[centroidIndex].Add(ParseDoubles(_data[i]));
             distances.Clear();
         }
     }
 
+    // making new centroids (new points)
+    private List<string> NewCentroids(Dictionary<int, List<List<double>>> dictionary)
+    {
+        List<string> newCentroids = new List<string>();
+        double sum = 0;
+        foreach (var pair in dictionary)
+        {
+            List<List<double>> lines = pair.Value;
+            var newPoints = new List<double>();
+            for (int i = 0; i < lines[0].Count; i++)
+            {
+                foreach (var t in lines)
+                {
+                    sum += t[i];
+                }
+                newPoints.Add(sum/lines.Count);
+                sum = 0;
+            }
+            newCentroids.Add(string.Join(",", newPoints.Select(x => x.ToString(CultureInfo.InvariantCulture))));
+        }
+        return newCentroids;
+    }
+
+    // returns List of doubles that contains 
+    // index 0: value
+    // index 1: index
+    // of the lowest number from distances
     private List<double> SortWithIndex(List<double> distances)
     {
         List<Tuple<double, int>> tmp = new List<Tuple<double, int>>();
@@ -59,6 +150,7 @@ public class Kmeans
         return indexAndValue;
     }
 
+    // counting euclidean distance from points from "line" by function "ParseDoubles"
     private List<double> Distances(string line)
     {
         double distance;
@@ -72,19 +164,25 @@ public class Kmeans
         return distances;
     }
 
-    // dorobic unikalnosc liczb losowych
+    // generating "_k" random (uniq) centroids from data 
     private void GenerateCentroids()
     {
         _centroids = new List<string>();
-        int index;
-        for (int i = 0; i < _k; i++)
+        HashSet<int> numbers = new HashSet<int>();
+        Random random = new Random();
+        while (numbers.Count < _k)
         {
-            index = new Random().Next(_data.Count - 1);
-            _centroids.Add(_data[index]);
-            _data.RemoveAt(index);
+            int randomNumber = random.Next(0, _data.Count - 1);
+            numbers.Add(randomNumber);
+        }
+        foreach (int number in numbers)
+        {
+            _centroids.Add(_data[number]);
+            _data.RemoveAt(number);
         }
     }
 
+    // Parsing doubles and changing data to correct form
     private List<double> ParseDoubles(string line)
     {
         List<string> values = line.Split(',').ToList();
--- k_means/k_means/Program.cs
+++ k_means/k_means/Program.cs
@@ -4,8 +4,12 @@
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Wybierz ilosc centroidow");
+            int i = int.Parse(Console.ReadLine() ?? string.Empty);
+            
             List<string> data = File.ReadAllLines("J:\\NAI_ai\\NAI_ai\\k_means\\k_means\\iris.data").ToList();
-            Kmeans kmeans = new Kmeans(4, data);
+            Kmeans kmeans = new Kmeans(i, data);
+            kmeans.DoKmeans();
         }
     }
 }
